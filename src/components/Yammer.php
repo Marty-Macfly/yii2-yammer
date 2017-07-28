e7af9acdbf81389626ddb4229df4d444978b81f5
Charles DELFLY <c.delfly@linkbynet.com>
2017-07-28 08:58:38 +0200

    Update log and add support for og

--- src/components/Yammer.php
+++ src/components/Yammer.php
@@ -6,10 +6,12 @@ use Yii;
 use yii\httpclient\Client;
 use yii\helpers\ArrayHelper;
 
+use macfly\yammer\models\OpenGraph;
+
 class Yammer extends Client
 {
-    public $baseUrl = 'https://www.yammer.com/api/v1';
-    public $token   = null;
+    public $baseUrl     = 'https://www.yammer.com/api/v1';
+    public $token       = null;
 
     public function beforeSend($request)
     {
@@ -31,27 +33,23 @@ class Yammer extends Client
     private function array_prefix($array, $prefix = '')
     {
         $array = array_flip($array);
-        $array = array_map('sprintf', array_fill(0, count($array), "%s%d"), array_fill(0, count($array), $prefix), $array);
-/*
         array_walk($array, function (&$v, $k, $prefix) {
             $v = sprintf("%s%d", $prefix,$v + 1);
         }, $prefix);
-*/
         return array_flip($array);
     }
 
     public function sendFiles($files)
     {
         $requests  = array_map(function ($file) {
-            return $this->post('/pending_attachments')
+            return $this->post('pending_attachments')
                 ->addFile('attachment', $file);
         }, $this->to_array($files));
 
         $callback   = function ($rs) {
             if(!($rs->isOk && ($id = ArrayHelper::getValue($rs->data, 'id')) !== null))
             {
-                print_r($rs->getHeaders());
-                //\Yii::error(sprintf("[%s %s] failed %d => %s", $method, $uri, $rs->statusCode, $rs->content));
+                \Yii::error(sprintf("[%s] failed %d => %s", $rs->headers->get('location'), $rs->statusCode, $rs->content));
             }
             return $id;
         };
@@ -67,7 +65,7 @@ class Yammer extends Client
     public function deleteFiles($ids)
     {
         $requests  = array_map(function ($id) {
-            return $this->delete(sprintf("/uploaded_files/%d", $id));
+            return $this->delete(sprintf("uploaded_files/%d", $id));
         }, $this->to_array($ids));
 
         return $this->batchRequest($requests);
@@ -86,73 +84,74 @@ class Yammer extends Client
         ];
     }
 
-    public function sendMessageToNetworks($ids, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToNetworks($ids, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         $ids        = $this->to_array($ids);
         $results    = [];
         foreach($ids as $id)
         {
-            $results[$id] = $this->sendMessageToNetwork($id, $body, $title, $tags, $files, $options);
+            $results[$id] = $this->sendMessageToNetwork($id, $body, $title, $og, $tags, $files, $options);
         }
         return $results;
     }
 
-    public function sendMessageToNetwork($id, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToNetwork($id, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         if($id !== null)
         {
             $options['network_id'] = $id;
         }
 
-        return $this->sendMessage($body, $title, $tags, $files, $options);
+        return $this->sendMessage($body, $title, $og, $tags, $files, $options);
     }
 
-    public function sendMessageToGroups($ids, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToGroups($ids, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         $ids        = $this->to_array($ids);
         $results    = [];
         foreach($ids as $id)
         {
-            $results[$id] = $this->sendMessageToGroup($id, $body, $title, $tags, $files, $options);
+            $results[$id] = $this->sendMessageToGroup($id, $body, $title, $og, $tags, $files, $options);
         }
         return $results;
     }
 
-    public function sendMessageToGroup($id, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToGroup($id, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         $options['group_id'] = $id;
-        return $this->sendMessage($body, $title, $tags, $files, $options);
+        return $this->sendMessage($body, $title, $og, $tags, $files, $options);
     }
 
-    public function sendMessageToUsers($ids, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToUsers($ids, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
-        return $this->sendMessageToUser($ids, $body, $title, $tags, $files, $options);
+        return $this->sendMessageToUser($ids, $body, $title, $og, $tags, $files, $options);
     }
 
-    public function sendMessageToUser($id, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToUser($id, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         $options['direct_to_user_ids'] = $this->to_array($id);
-        return $this->sendMessage($body, $title, $tags, $files, $options);
+        return $this->sendMessage($body, $title, $og, $tags, $files, $options);
     }
 
-    public function sendMessageToMessages($ids, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToMessages($ids, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         $ids        = $this->to_array($ids);
         $results    = [];
         foreach($ids as $id)
         {
-            $results[$id] = $this->sendMessageToMessage($id, $body, $title, $tags, $files, $options);
+            $results[$id] = $this->sendMessageToMessage($id, $body, $title, $og, $tags, $files, $options);
         }
         return $results;
     }
 
-    public function sendMessageToMessage($id, $body, $title = null, $tags = [], $files = [], $options = [])
+    public function sendMessageToMessage($id, $body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         $options['replied_to_id'] = $id;
-        return $this->sendMessage($body, $title, $tags, $files, $options);
+        return $this->sendMessage($body, $title, $og, $tags, $files, $options);
     }
 
-    public function sendMessage($body, $title = null, $tags = [], $files = [], $options = [])
+    //
+    public function sendMessage($body, $title = null, OpenGraph $og = null, $tags = [], $files = [], $options = [])
     {
         $tags   = $this->to_array($tags);
         $files  = $this->to_array($files);
@@ -166,25 +165,30 @@ class Yammer extends Client
             $data['title'] = $title;
         }
 
+        if($og !== null)
+        {
+            $data = array_merge($data, array_filter($og->toArray(), function($v) {
+                return $v !== null;
+            }));
+        }
+
         $data   = array_merge(
             $data,
             $this->array_prefix(array_slice($tags, 0, $length), 'topic'),
             $this->array_prefix(array_values($this->sendFiles(array_slice($files, 0, $length))), 'pending_attachment'),
             $options
         );
-
+        $rs     = $this->post('messages.json', $data)->send();
         print_r($data);
-
-        $rs = $this->post('/messages.json', $data)->send();
 
         if($rs->isOk && ($id = ArrayHelper::getValue($rs->data, 'messages.0.id')) !== null)
         {
-            echo $id . "\n";
             if(max(ceil(count($tags) / $length), ceil(count($files) / $length)) > 1)
             {
                 $this->sendMessageToMessage(
                     $id,
                     '',
+                    null,
                     null,
                     array_slice($tags, $length),
                     array_slice($files, $length),
@@ -195,23 +199,15 @@ class Yammer extends Client
         }
         else
         {
-            Yii::error(sprintf("Failed to send message status code: %s => %s", $rs->statusCode, $rs->content));
+            Yii::error(sprintf("[%s] failed %d => %s", $rs->headers->get('location'), $rs->statusCode, $rs->content));
             return false;
         }
-
-    //'og_url'            => 'http://www.linkbynet.com/',
-    //'og_fetch'          => 'true',
-    //'og_title'          => 'OG title',
-    //'og_site_name'      => 'Site name',
-    //'og_object_type'    => 'image',
-    //'og_description'    => 'Linkbynet site web',
-    //'og_image'          => 'https://a248.e.akamai.net/assets.github.com/images/modules/dashboard/octofication.png',
     }
 
     public function deleteMessages($ids)
     {
         $requests  = array_map(function ($id) {
-            return $this->delete(sprintf("/messages/%d", $id));
+            return $this->delete(sprintf("messages/%d", $id));
         }, $this->to_array($ids));
 
         return $this->batchRequest($requests);
@@ -231,8 +227,7 @@ class Yammer extends Client
             $callback = function ($rs) {
                 if(!$rs->isOk)
                 {
-                    print_r($rs->getHeaders());
-                    // \Yii::error(sprintf("[%s %s] failed %d => %s", $method, $uri, $rs->statusCode, $rs->content));
+                    \Yii::error(sprintf("[%s] failed %d => %s", $rs->headers->get('location'), $rs->statusCode, $rs->content));
                 }
                 return $rs->isOk;
             };
